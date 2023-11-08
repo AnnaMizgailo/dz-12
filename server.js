14fb88a9a83429ae1909ca28f6bf763f83478251
AnnaMizgailo <102236391+AnnaMizgailo@users.noreply.github.com>
2023-11-08 20:48:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-const express = require("express");
-const path = require("path");
 
-const app = express();
-
-app.use(express.static(path.join(__dirname, "./public")));
-app.set("view engine", "hbs");
-
-app.get("/", (_, res) => {
-    res.render("info.hbs", {
-        itemVisible: false
-    });
-});
